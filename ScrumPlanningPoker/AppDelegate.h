e8613f1cdbd14dfa2f375adf447f550ded64ac18
alex-kepar <alex.kepar@gmail.com>
2014-02-17 15:49:05 +0200

    Initial Commit

---
+++ ScrumPlanningPoker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrumPlanningPoker
+//
+//  Created by Administrator on 2/17/14.
+//  Copyright (c) 2014 Delphi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrumPlanningPoker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScrumPlanningPoker
+//
+//  Created by Administrator on 2/17/14.
+//  Copyright (c) 2014 Delphi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
